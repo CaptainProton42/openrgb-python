24ba80bde25ec3b886aa01b12d41541ffdfe4771
John Wigg <git@john-wigg.dev>
2023-09-22 22:51:20 +0100

    Add support for profile listing/loading in effects plugin.

--- openrgb/plugins/effects.py
+++ openrgb/plugins/effects.py
@@ -9,6 +9,8 @@ class EffectPacketType(IntEnum):
     REQUEST_EFFECT_LIST = 0
     START_EFFECT = 20
     STOP_EFFECT = 21
+    REQUEST_PROFILE_LIST = 22
+    LOAD_PROFILE = 23
 
 
 @dataclass
@@ -29,6 +31,17 @@ class Effect:
             enabled
         )
 
+@dataclass
+class Profile:
+    name: str
+
+    @classmethod
+    def unpack(cls, data: Iterator[int], version: int, *args) -> Profile:
+        name = utils.parse_string(data)
+
+        return cls(
+            name
+        )
 
 class EffectsPlugin(ORGBPlugin):
     version = 1
@@ -41,10 +54,15 @@ class EffectsPlugin(ORGBPlugin):
     def update(self):
         self.send_packet(EffectPacketType.REQUEST_EFFECT_LIST, request=True)
         self.comms.read()
+        self.send_packet(EffectPacketType.REQUEST_PROFILE_LIST, request=True)
+        self.comms.read()
 
     def recv(self, pkt_id: EffectPacketType, data: Iterator[int]):  # type: ignore
         if pkt_id == EffectPacketType.REQUEST_EFFECT_LIST:
             self.effects = utils.parse_list(Effect, data, self.version)
+        elif pkt_id == EffectPacketType.REQUEST_PROFILE_LIST:
+            self.profiles = utils.parse_list(Profile, data, self.version)
+            pass
 
     def start_effect(self, effect: Union[int, str, Effect]):
         if type(effect) == int:
@@ -69,3 +87,15 @@ class EffectsPlugin(ORGBPlugin):
         data = utils.pack_string(effect.name)  # type: ignore
 
         self.send_packet(EffectPacketType.STOP_EFFECT, data)
+
+    def load_profile(self, profile: Union[int, str, Effect]):
+        if type(profile) == int:
+            effect = self.effects[effect]  # type: ignore
+        elif type(profile) == str:
+            try:
+                profile = next(m for m in self.profiles if m.name.lower() == profile.lower())  # type: ignore
+            except StopIteration as e:
+                raise ValueError(f"Profile `{profile}` not found") from e
+        data = utils.pack_string(profile.name)  # type: ignore
+
+        self.send_packet(EffectPacketType.LOAD_PROFILE, data)
